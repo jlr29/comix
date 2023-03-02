d508f20d6e68d8d3df4fbbf7da65df45d646651c
jlr29 <giova9975@gmail.com>
2023-03-02 22:52:04 +0100

    birthday app trial

--- app/src/main/java/com/jlrone/comix/MainActivity.kt
+++ app/src/main/java/com/jlrone/comix/MainActivity.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jlrone.comix.ui.theme.ComixTheme
 
@@ -27,7 +29,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayCardWithPic("Angurie","- Da Guiova")
+                    BirthdayCardWithPic(stringResource(R.string.happy_birthday_text),
+                        stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -35,17 +39,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayCard(message: String, from: String) {
-    Column{
-        Text(text = message, fontSize = 36.sp)
-        Text(text = from, fontSize = 24.sp)
+fun BirthdayCard(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 28.dp)
+        )
+        Text(text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp, end = 16.dp)
+                .align(alignment = Alignment.End)
+        )
     }
 }
 @Composable
-fun BirthdayCardWithPic(message: String, from: String, modifier: Modifier = Modifier) {
+fun BirthdayCardWithPic(message: String, from: String) {
     val image = painterResource(R.drawable.androidparty)
     Box {
-        Image(painter = image, contentDescription = null)
+        Image(painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop)
         BirthdayCard(message = message, from = from)
     }
 }
